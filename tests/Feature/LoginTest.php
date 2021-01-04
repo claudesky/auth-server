4f4c02a81bee7c3a839a77fc1369aacaff39cb36
Claude Kirke <claudekirke@gmail.com>
2021-01-05 00:39:04 +0800

    test: add cookie tests to login feature test

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -65,4 +65,50 @@ class LoginTest extends TestCase
 
         $this->assertGuest();
     }
+
+    /**
+     * Test that valid cookie is set on successful login.
+     *
+     * @return void
+     */
+    public function testValidLoginSetsCookie()
+    {
+        $user = User::factory()
+            ->create();
+
+        $response = $this->json(
+            'POST',
+            '/login',
+            [
+                'email' => $user->email,
+                'password' => 'password'
+            ]
+        );
+
+        $response->assertCookie(config('session.cookie'));
+    }
+
+    /**
+     * Test that no cookie is set on failed login.
+     *
+     * @return void
+     */
+    public function testInvalidLoginDoesNotSetCookie()
+    {
+        $user = User::factory()
+            ->create();
+
+        $response = $this->json(
+            'POST',
+            '/login',
+            [
+                'email' => $user->email,
+                'password' => 'wrongpassword'
+            ]
+        );
+
+        $response->assertCookieMissing(config('session.cookie'));
+    }
+
+
 }
