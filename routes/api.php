3d38fc7e6056f88db67a2970d2991b77819c7061
Claude Kirke <claudekirke@gmail.com>
2021-01-04 12:12:44 +0800

    feat (routes): add basic login route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -23,6 +25,8 @@ Route::get('/test', function() {
         'hello' => 'world!'
     ];
 });
+
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
