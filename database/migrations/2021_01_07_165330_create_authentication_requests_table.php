ecc5798a12f81b324097cbff693d2a01702ee2cf
Claude Kirke <claudekirke@gmail.com>
2021-01-08 06:44:54 +0800

    feat (database): add authentication_requests table migration

---
+++ database/migrations/2021_01_07_165330_create_authentication_requests_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthenticationRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authentication_requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('nonce', 63)
+                ->index();
+            $table->string('status', 31)
+                ->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authentication_requests');
+    }
+}
