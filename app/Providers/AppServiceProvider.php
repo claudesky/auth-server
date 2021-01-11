6449f1766b21fb7a8a353ae01c1e4d750625ae91
Claude Kirke <claudekirke@gmail.com>
2021-01-11 11:48:30 +0800

    feat (providers): add cache fetching for authorization sources fetching from cache to AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $authorization_sources = Cache::get('authorization_sources');
+
+        config([
+            'services' => $authorization_sources
+        ]);
     }
 }
