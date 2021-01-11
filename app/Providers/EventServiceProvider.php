7e6f807d661890ffbb7eb5ee93e8f3bfc95f1594
Claude Kirke <claudekirke@gmail.com>
2021-01-11 11:49:51 +0800

    feat (providers): register auth_source and auth_request observers in EventServiceProvider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,13 @@
 
 namespace App\Providers;
 
+use App\Models\AuthorizationSource;
+use App\Models\AuthenticationRequest;
+use App\Observers\AuthorizationSourceObserver;
+use App\Observers\AuthenticationRequestObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -27,6 +30,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        AuthorizationSource::observe(AuthorizationSourceObserver::class);
+        AuthenticationRequest::observe(AuthenticationRequestObserver::class);
     }
 }
